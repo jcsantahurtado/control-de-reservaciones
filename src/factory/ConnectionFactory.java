b5c45667f0a81b6e05ed9fbe026189ee74fd3691
Juan Santa <jcsantahurtado@gmail.com>
2022-06-20 16:22:34 -0500

    Working with the connection pool

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -1,20 +1,30 @@
 package factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	private DataSource dataSource;
+
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource
+				.setJdbcUrl("jdbc:mysql://localhost/control_de_reservaciones?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("root1234");
+		pooledDataSource.setMaxPoolSize(10);
+
+		this.dataSource = pooledDataSource;
+	}
+
 	public Connection recuperaConexion() throws SQLException {
 
-		return DriverManager.getConnection(
-
-				"jdbc:mysql://localhost/control_de_reservaciones?useTimeZone=true&serverTimeZone=UTC",
-
-				"root",
-
-				"root1234");
+		return this.dataSource.getConnection();
 	}
 
 }
